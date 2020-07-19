c31038e452911b55f3054c6c2fcab93f7dc0da52
gonzabl <gonzabl@live.com.ar>
2020-07-19 04:03:39 -0300

    Nueva distribucion de la pagina

---
+++ src/Componentes/Computadora.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Computadora(){
+
+    const [seleccion,setSeleccion] = useState("");
+
+    return (<div></div>);
+} 
+
+export default Computadora;
--- src/Game.js
+++ src/Game.js
@@ -4,16 +4,14 @@ import reglas from './img/reglas.png'
 
 function Game() {
   return (
-    <div class="wrapper">
-      <div class="box a">Header</div>
-      <div class="box b">Ganador: " "</div>
-      <div class="box c">
-          <h3>Reglas:</h3> 
-          <img src={reglas} className="reglas" alt="reglas" />
-      </div>
-      <div class="box d">Jugador</div>
-      <div class="box e">Computadora</div>
-    </div>
+    <div class="grid-container">
+        <div class="item1">Bienvenidos a Piedra, Papel, Tijera, Lagarto o Spock!</div>
+        <div class="item2"><h3>Reglas:</h3> 
+          <img src={reglas} className="reglas" alt="reglas" /></div>
+        <div class="item3">Jugador</div>  
+        <div class="item4">Computadora</div>
+        <div class="item5">Ganador: " "</div>
+  </div>
   );
 }
 
